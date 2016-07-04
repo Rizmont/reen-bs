8978e1c3ced1d5a63bcb5a776a5a7bd4155cac0f
Slava Rizmont <vrizmont@mail.ru>
2016-07-04 23:03:08 +0300

    start

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//
+
+function setEqualHeight(columns)
+{
+var tallestcolumn = 0;
+columns.each(
+function()
+{
+currentHeight = $(this).height();
+if(currentHeight > tallestcolumn)
+{
+tallestcolumn = currentHeight;
+}
+}
+);
+columns.height(tallestcolumn);
+}
+
+$(document).ready(function() {
+    setEqualHeight($(".item"));
+
+});
+
+
+$(window).resize(function() {
+    setEqualHeight($(".item"));
+
+});
